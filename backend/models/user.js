e60a06efa09dc250f9bfc1e9ff8fc0a15c7849f3
GogulaSivaGanesh <21A91A05D4@aec.edu.in>
2024-03-18 23:37:45 +0530

    first commit

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema({
+    username:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+});
+
+export default mongoose.model('User', User);
